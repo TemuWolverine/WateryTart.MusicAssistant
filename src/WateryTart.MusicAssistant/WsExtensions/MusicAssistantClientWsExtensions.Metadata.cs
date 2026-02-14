288fa4a657365bec10cfb339b77ef042c724fc56
Paul Jenkins <paul@thewoodknight.com>
2026-02-14 11:43:59 +1100

    Fix: Corrected name for internal class (MediaAssistantJsonContext -> MusicAssistantJsonContext) Feat: Added lyric fetching, ex var trackRpc = await client.WithRpc().GetLibraryItemAsync(MediaType.Track, "2500", "library"); var lyrics = await client.WithRpc().GetLyricsAsync(trackRpc);
    Works for both WithWs and WithRpc

--- src/WateryTart.MusicAssistant/WsExtensions/MusicAssistantClientWsExtensions.Metadata.cs
+++ src/WateryTart.MusicAssistant/WsExtensions/MusicAssistantClientWsExtensions.Metadata.cs
@@ -1,7 +1,22 @@
-﻿namespace WateryTart.MusicAssistant.WsExtensions;
+﻿using WateryTart.MusicAssistant.Messages;
+using WateryTart.MusicAssistant.Models;
+using WateryTart.MusicAssistant.Models.Enums;
+using WateryTart.MusicAssistant.Responses;
+
+namespace WateryTart.MusicAssistant.WsExtensions;
 
 public static partial class MusicAssistantClientWsExtensions
 {
     /*MusicAssistantClientWs*/
-
+    public static async Task<StringArrayResponse> GetLyricsAsync(this MusicAssistantClientWs c, Item track)
+    {
+        var m = new Message(Commands.MetadataGetTrackLyrics)
+        {
+            args = new Dictionary<string, object>()
+                {
+                    { "track", track }
+                }
+        };
+        return await SendAsync<StringArrayResponse>(c, m);
+    }
 }

29a94de2fc5385dadaed14ba873234a902490f55
Paul Jenkins <paul@thewoodknight.com>
2026-03-03 08:23:11 +1100

    fix: OrderBy enum now gains descriptions

--- src/WateryTart.MusicAssistant/Models/Enums/OrderBy.cs
+++ src/WateryTart.MusicAssistant/Models/Enums/OrderBy.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 using WateryTart.MusicAssistant.Converters;
 
 namespace WateryTart.MusicAssistant.Models.Enums;
@@ -7,16 +8,37 @@ namespace WateryTart.MusicAssistant.Models.Enums;
 public enum OrderBy
 {
     Unknown,
+    [Description("Alphabetical (A-Z)")]
     name,
+
+    [Description("Alphabetical (Z-A)")]
     name_desc,
+
+    [Description("Sort Name (A-Z)")]
     sort_name,
+
+    [Description("Sort Name (Z-A)")]
     sort_name_desc,
+
+    [Description("Timestamp Added")]
     timestamp_added,
+    [Description("Timestamp Added (Desc)")]
     timestamp_added_desc,
+
+    [Description("Last Played")]
     last_played,
+    [Description("Last Played (Desc)")]
     last_played_desc,
+
+    [Description("Play Count")]
     play_count,
+
+    [Description("Play Count (Desc)")]
     play_count_desc,
+
+    [Description("Duration")]
     duration,
+
+    [Description("Duration (Desc)")]
     duration_desc
 }
